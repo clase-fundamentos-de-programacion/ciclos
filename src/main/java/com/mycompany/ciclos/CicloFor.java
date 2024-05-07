a148e909da0fe76ee6ab5fd926f44854a8c193f2
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-05-06 20:49:36 -0500

    Ejemplos de ciclos en Java.

---
+++ src/main/java/com/mycompany/ciclos/CicloFor.java
@@ -0,0 +1,14 @@
+package com.mycompany.ciclos;
+
+public class CicloFor {
+    public static void main(String[] args) {
+        for(int i = 1 ;i <= 100 ; i = i + 1){
+            System.out.println("Número " + i);
+        }
+    }
+}
+
+// Imprimir los números impares del 1 al 100
+// Imprimir los números pares del 1 al 100
+// Imprimir los números del 100 al 1
+// Imprimir las potencias de 2 del 1 al 100
---
+++ src/main/java/com/mycompany/ciclos/CicloWhile.java
@@ -0,0 +1,16 @@
+package com.mycompany.ciclos;
+
+public class CicloWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 100){
+            System.out.println("Número " + i);
+            i = i + 1;
+        }
+    }
+}
+
+// Imprimir los números impares del 1 al 100
+// Imprimir los números pares del 1 al 100
+// Imprimir los números del 100 al 1
+// Imprimir las potencias de 2 del 1 al 100
